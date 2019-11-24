5fb1eeb6b4004e51cef073b3c91747f009b96a9d
张旭 <zhangxu@udesk.cn>
2019-11-24 21:12:47 +0800

    chore: update travis ci

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,8 +4,8 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base')
   
   it('entry',() => {
-    assert.equal(baseConfig.entry.search, '/Users/john/Documents/execises/webpack_test/test/smoke/template/src/search/index.js')
+    assert.equal(baseConfig.entry.search.indexOf('/test/smoke/template/src/search/index.js') > -1, true)
 
-    assert.equal(baseConfig.entry.vedio, '/Users/john/Documents/execises/webpack_test/test/smoke/template/src/vedio/index.js')
+    assert.equal(baseConfig.entry.vedio.indexOf('/test/smoke/template/src/vedio/index.js') > -1, true)
   })
 }) 
